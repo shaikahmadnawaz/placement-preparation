279f97951870eef062aea71aeb721fd6f73140e5
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-03 16:41:29 +0530

    K-Concatenation Maximum Sum

---
+++ README.md
@@ -0,0 +1,3 @@
+# Placement Prepration
+
+Hello everyone, I have created this repository for DSA questions and for placements preparation.
---
+++ arrays/K-ConcatenationMaximumSum.java
@@ -0,0 +1,28 @@
+package arrays;
+
+class KConcatenationMaximumSum {
+  public int kConcatenationMaxSum(int[] arr, int k) {
+    int mod = 1000000007;
+    int n = arr.length;
+    long presum = 0, premax = 0, sufsum = 0, sufmax = 0, cursum = 0, curmax = 0;
+    for (int i = 0; i < n; i++) {
+
+      // prefix sum
+      presum += arr[i];
+      premax = Math.max(premax, presum);
+
+      // suffix sum
+      sufsum += arr[n - 1 - i];
+      sufmax = Math.max(sufmax, sufsum);
+
+      // Kadane's Algorithm
+      cursum = Math.max(0, cursum) + arr[i];
+      curmax = Math.max(curmax, cursum);
+
+    }
+    long max = Math.max(curmax, premax + sufmax + Math.max(0, presum * (k - 2))); // prefix sum(presum) will be the sum
+                                                                                  // of the complete array after entire
+                                                                                  // traversal...
+    return (int) ((k == 1 ? curmax : max) % mod);
+  }
+}
