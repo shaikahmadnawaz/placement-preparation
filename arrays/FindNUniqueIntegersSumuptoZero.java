b62d02041229a38c06ce792a440c4126fec1a4f2
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-17 12:47:42 +0530

    Find N Unique Integers Sum up to Zero

---
+++ arrays/FindNUniqueIntegersSumuptoZero.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int[] sumZero(int n) {
+    int[] res = new int[n];
+    int left = 0, right = n - 1, start = 1;
+    while (left < right) {
+      res[left++] = start;
+      res[right--] = -start;
+      start++;
+    }
+    return res;
+  }
+}
