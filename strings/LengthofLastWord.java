2cbac77a7286cd8fc2c81318d611a1fb1dd0c3ea
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-07 22:59:49 +0530

    Length of Last Word

---
+++ strings/LengthofLastWord.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int lengthOfLastWord(String s) {
+    String sp = s.trim();
+    int count = 0;
+    for (int i = sp.length() - 1; i >= 0; i--) {
+      if (sp.charAt(i) != ' ') {
+        count++;
+      } else {
+        break;
+      }
+    }
+    return count;
+  }
+}
